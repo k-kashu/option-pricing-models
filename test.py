e022c558ffb5de07a77ab4e2704fe3b98e09666e
k-kashu <kkashu.hit.62@gmail.com>
2026-09-13 14:21:13 +0900

    Initial commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import sys
+import pandas as pd
+import numpy as np
+
+# 現在使われているPythonの実行パスを出力
+print("Python Path:", sys.executable)
+print("Pandas Version:", pd.__version__)
+print("NumPy Version:", np.__version__)
